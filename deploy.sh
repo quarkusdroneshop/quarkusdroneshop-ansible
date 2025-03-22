2c06441d5f4f360b6c8a7a242b0fffa65bcd30af
Noriaki Mushino <nmushino@redhat.com>
2025-03-23 08:37:11 +0900

    Add deployment.yaml

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,30 @@ oc delete all -l app=kitchen
 oc delete all -l app=barista
 oc delete all -l app=counter
 
-oc new-app ubi8/openjdk-17~https://github.com/nmushino/quarkuscoffeeshop-counter.git --name=counter --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
-
 oc apply -f configmap/coffeeshop-configmap.yaml -n quarkuscoffeeshop-demo
+
+# counter app install
+oc new-app ubi8/openjdk-17~https://github.com/nmushino/quarkuscoffeeshop-counter.git --name=counter --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
 oc apply -f configmap/counter-development.yaml -n quarkuscoffeeshop-demo
 
-#oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-barista.git --name=barista --allow-missing-images --strategy=source
-#oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-kitchen.git --name=kitchen --allow-missing-images --strategy=source
-#oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-web.git --name=web --allow-missing-images --strategy=source
+# barista app install
+oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-barista.git --name=barista --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
+oc apply -f configmap/barista-development.yaml -n quarkuscoffeeshop-demo
 
+# kitchen app install
+oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-kitchen.git --name=kitchen --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
+oc apply -f configmap/kitchen-development.yaml -n quarkuscoffeeshop-demo
+
+# web app install
+oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-web.git --name=web --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
+oc apply -f configmap/web-development.yaml -n quarkuscoffeeshop-demo
+
+
+
+oc expose deployment web --port=8080 --name=quarkuscoffeeshop-web
+#oc expose deployment kitchen --port=8080 --name=quarkuscoffeeshop-web
+#oc expose deployment barista --port=8080 --name=quarkuscoffeeshop-web
+#oc expose deployment counter --port=8080 --name=quarkuscoffeeshop-web
+
+oc expose svc/quarkuscoffeeshop-web
+oc expose svc/quarkuscoffeeshop-customermocker
