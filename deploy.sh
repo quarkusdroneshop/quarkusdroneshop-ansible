82e1594007f3419e0b845aec87cab5457dd19b4f
Noriaki Mushino <nmushino@redhat.com>
2025-03-22 15:59:35 +0900

    update postgres version

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 oc delete project quarkuscoffeeshop-demo
 
-podman build -t quarkuscoffeeshop ../quarkuscoffeeshop-ansible
+podman build -t quarkuscoffeeshop .
 podman run --platform linux/amd64 -it --env-file=./source.env quarkuscoffeeshop
 
 oc delete all -l app=web
@@ -8,10 +8,10 @@ oc delete all -l app=kitchen
 oc delete all -l app=barista
 oc delete all -l app=counter
 
-oc new-app ubi8/openjdk-17~https://github.com/nmushino/quarkuscoffeeshop-counter.git --name=counter --allow-missing-images --strategy=source
+oc new-app ubi8/openjdk-17~https://github.com/nmushino/quarkuscoffeeshop-counter.git --name=counter --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
 
-oc apply -f configmap/coffeeshop-configmap.yaml
-oc apply -f configmap/counter-buildconfig.yaml
+oc apply -f configmap/coffeeshop-configmap.yaml -n quarkuscoffeeshop-demo
+oc apply -f configmap/counter-development.yaml -n quarkuscoffeeshop-demo
 
 #oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-barista.git --name=barista --allow-missing-images --strategy=source
 #oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-kitchen.git --name=kitchen --allow-missing-images --strategy=source
