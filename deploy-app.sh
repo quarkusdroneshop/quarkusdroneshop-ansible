0b5f566a59ebcaf40e33efe42d7835c3efa80297
Noriaki Mushino <nmushino@redhat.com>
2025-03-23 12:11:57 +0900

    Update Configmap

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,5 @@
 
-oc delete all -l app=web
+#oc delete all -l app=web
 #oc delete all -l app=kitchen
 #oc delete all -l app=barista
 #oc delete all -l app=counter
@@ -19,9 +19,8 @@ oc apply -f configmap/coffeeshop-configmap.yaml -n quarkuscoffeeshop-demo
 #oc apply -f configmap/kitchen-development.yaml -n quarkuscoffeeshop-demo
 
 # web app install
-oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-web.git --name=web --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
-oc apply -f configmap/web-development.yaml -n quarkuscoffeeshop-demo
-
+#oc new-app ubi8/openjdk-11~https://github.com/nmushino/quarkuscoffeeshop-web.git --name=web --allow-missing-images --strategy=source -n quarkuscoffeeshop-demo
+#oc apply -f configmap/web-development.yaml -n quarkuscoffeeshop-demo
 
 
 oc expose deployment web --port=8080 --name=quarkuscoffeeshop-web
@@ -31,3 +30,16 @@ oc expose deployment web --port=8080 --name=quarkuscoffeeshop-web
 
 oc expose svc/quarkuscoffeeshop-web
 #oc expose svc/quarkuscoffeeshop-customermocker
+
+
+###### update configmap
+CLUSTER_DOMAIN=$(oc get routes -A --no-headers | awk '{print $3}' | grep -m1 web)
+CORS_ORIGINS_URL="http://${CLUSTER_DOMAIN}"
+oc patch configmap coffeeshop-config --type merge -p "{\"data\":{\"CORS_ORIGINS\":\"$CORS_ORIGINS_URL\"}}"
+
+STREAM_URL="http://${CLUSTER_DOMAIN}/dashboard/stream"
+oc patch configmap coffeeshop-config --type merge -p "{\"data\":{\"STREAM_URL\":\"$STREAM_URL\"}}"
+
+LOYALTY_STREAM_URL="http://${CLUSTER_DOMAIN}/dashboard/loyaltystream"
+oc patch configmap coffeeshop-config --type merge -p "{\"data\":{\"LOYALTY_STREAM_URL\":\"$STREAM_URL\"}}"
+
