0e13e16bacf4955c3ae49d8aeeafaf6a1d5ac4a5
Noriaki Mushino <nmushino@redhat.com>
2025-06-02 08:25:24 +0900

    Update posgres yaml file

--- files/initdb_postgres_custom.sh
+++ files/initdb_postgres_custom.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# postgresql.confのshared_preload_librariesを設定
-echo "shared_preload_libraries = 'pg_stat_statements,auto_explain,pgaudit'" >> /pgdata/pg17/postgresql.conf
-
 ### openmetadataで必要となるため、管理者権限で実行する
 export PGPASSWORD=$(oc get secret coffeeshopdb-pguser-postgres -n quarkuscoffeeshop-demo -o jsonpath='{.data.password}' | base64 -d)
-export PGHOSTNAME=$(oc get secret coffeeshopdb-pguser-postgres -n quarkuscoffeeshop-demo -o jsonpath='{.data.host}' | base64 -d)
+#export PGHOSTNAME=$(oc get secret coffeeshopdb-pguser-postgres -n quarkuscoffeeshop-demo -o jsonpath='{.data.host}' | base64 -d)
+psql -U postgres -h ${PGHOSTNAME} -c "SHOW shared_preload_libraries;"
 psql -U postgres -h ${PGHOSTNAME} -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"
