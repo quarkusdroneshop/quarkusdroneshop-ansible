71230010ae79371b17bd8da727d4f874a31aeb66
Noriaki Mushino <nmushino@redhat.com>
2025-12-28 14:04:59 +0900

    Update for postgres-customs

--- files/init_postgres_custom.sh
+++ files/init_postgres_custom.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 ### openmetadataで必要となるため、管理者権限で実行する
-export PGPASSWORD=$(oc get secret droneshopdb-pguser-postgres -n quarkusdroneshop-demo -o jsonpath='{.data.password}' | base64 -d)
-export PGHOSTNAME=$(oc get secret droneshopdb-pguser-postgres -n quarkusdroneshop-demo -o jsonpath='{.data.host}' | base64 -d)
+export PGHOSTNAME="droneshopdb-primary.quarkusdroneshop-demo.svc"
+export PGPASSWORD="${PGPASSWORD}"
+export PGPORT="5432"
+export PGUSER="droneshopadmin"
+export PGDATABASE="droneshopdb"
 
 psql -U postgres -h ${PGHOSTNAME} -c "SHOW shared_preload_libraries;"
 psql -U postgres -h ${PGHOSTNAME} -d droneshopdb -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"
