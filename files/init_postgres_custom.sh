55c03f71f3e024834acc91216717a48a236d0338
Noriaki Mushino <nmushino@redhat.com>
2026-06-06 09:29:30 +0900

    Update of Create Table

--- files/init_postgres_custom.sh
+++ files/init_postgres_custom.sh
@@ -2,11 +2,10 @@
 
 ### openmetadataで必要となるため、管理者権限で実行する
 export PGHOSTNAME="droneshopdb-primary.quarkusdroneshop-demo.svc"
-export PGPASSWORD="${PGPASSWORD}"
+export PGPASSWORD="postgres"
 export PGPORT="5432"
-export PGUSER="droneshopadmin"
-export PGDATABASE="droneshopdb"
+export PGUSER="postgres"
 
-psql -h ${PGHOSTNAME} -d ${PGDATABASE} -c "SHOW shared_preload_libraries;"
-psql -h ${PGHOSTNAME} -d ${PGDATABASE} -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"
-psql -h ${PGHOSTNAME} -d ${PGDATABASE} -c "GRANT pg_read_all_stats TO droneshopadmin;"
+psql -h ${PGHOSTNAME} -p 5432 -U ${PGUSER} droneshopdb -c "SHOW shared_preload_libraries;"
+psql -h ${PGHOSTNAME} -p 5432 -U ${PGUSER} droneshopdb -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"
+psql -h ${PGHOSTNAME} -p 5432 -U ${PGUSER} droneshopdb -c "GRANT pg_read_all_stats TO droneshopadmin;"
